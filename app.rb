5bc81b96a409e8006b331c8d721c64803d8ac40a
Joy Liu <joy@joy-liu.com>
2014-08-13 18:43:05 -0700

    store username and password in ENV variables!

--- app.rb
+++ app.rb
@@ -71,7 +71,7 @@ helpers do
 
   def authorized?
     @auth ||=  Rack::Auth::Basic::Request.new(request.env)
-    @auth.provided? and @auth.basic? and @auth.credentials and @auth.credentials == ["admin", "wehaterecruiters!"]
+    @auth.provided? and @auth.basic? and @auth.credentials and @auth.credentials == [ENV["USERNAME"], ENV["PASSWORD"]]
   end
 end
 
